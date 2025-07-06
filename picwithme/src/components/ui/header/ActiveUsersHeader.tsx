d768c16160b5d6313a40dba652d7c81501a8e321
yook-jongho <dbrwhdgh1203@naver.com>
2025-07-07 02:02:14 +0900

    refactor/#14/Avatar 컴포넌트 활용

--- picwithme/src/components/ui/header/ActiveUsersHeader.tsx
+++ picwithme/src/components/ui/header/ActiveUsersHeader.tsx
@@ -1,5 +1,6 @@
 import type { User } from '@/types/room.ts';
 import logoImg from '@/assets/images/logo.png';
+import Avatar from '../Avatar';
 
 interface ActiveUsersHeaderProps {
   userList: User[];
@@ -8,10 +9,12 @@ interface ActiveUsersHeaderProps {
 const ActiveUsersHeader: React.FC<ActiveUsersHeaderProps> = ({ userList }) => {
   return (
     <div className='sticky top-0 z-20 flex h-16 w-full shrink-0 items-center justify-between bg-white px-4'>
-      <img src={logoImg} alt='Logo' className='logo' width={84} />
-      {userList.map((user) => (
-        <img className='h-12 w-12 rounded-full object-cover' id={user.id} src={user.name} alt='유저 썸네일' />
-      ))}
+      <img src={logoImg} alt='Logo' className='logo' width={40} />
+      <div className='flex flex-row gap-2'>
+        {userList.map((user) => (
+          <Avatar key={user.id} src={user.img} className='bg-warning' />
+        ))}
+      </div>
     </div>
   );
 };
